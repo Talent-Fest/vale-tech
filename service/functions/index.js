b17c3043b2134d8333435cd25ef90364f6690d8a
coderanac <coder.anac@gmail.com>
2020-09-09 11:13:22 -0300

    fix bug service

--- service/functions/index.js
+++ service/functions/index.js
@@ -1,15 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const cors = require('cors');
 const express = require('express');
-const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
-
+admin.initializeApp({
+  apiKey: "AIzaSyBECU7Xsk3l6NDBlxLV4MeKQh_zqjtsBZM",
+  authDomain: "valetech-5ea35.firebaseapp.com",
+  databaseURL: "https://valetech-5ea35.firebaseio.com",
+  projectId: "valetech-5ea35",
+  storageBucket: "valetech-5ea35.appspot.com",
+  messagingSenderId: "231084693612",
+  appId: "1:231084693612:web:8bb1bb24b990580fd9e049"
+});
 
 const app = express();
 app.use(cors({ origin: true }));
 
-app.get('/', (req, res) => {
+app.get('/urldahub', (req, res) => {
   res.status(200).send('ok');
 });
 
@@ -17,4 +24,4 @@ app.post('/', (req, res) => {
   res.status(200).send('ok');
 });
 
-functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
