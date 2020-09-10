062dce0eab1b27df24af288e776838237c6171f9
Thais Durynek <thais.durynek@gmail.com>
2020-09-10 13:01:38 -0300

    mudança do C para c nas rotas do account

--- client/src/Pages/Account/account.js
+++ client/src/Pages/Account/account.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../../Components/Header/index';
-import Footer from '../../Components/Footer/index';
-import Button from '../../Components/Button/index';
-import Input from '../../Components/Input/index';
+import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
+import Button from '../../components/Button/index';
+import Input from '../../components/Input/index';
 import Logo from '../../img/logoValeTech.png';
 import './account.css';
 
